644bce4de510cec801e529463756769e8eae96f1
nahidrahat <nahidrahat007@gmail.com>
2025-02-04 18:54:05 +0600

    updated

--- get_x/lib/counter_controller.dart
+++ get_x/lib/counter_controller.dart
@@ -1,14 +1,17 @@
+// Import the GetX package to use GetX functionalities
 import 'package:get/get.dart';
 
-class CounterController extends GetxController{
+// Define the CounterController class which extends GetXController
+class CounterController extends GetxController {
+  // Private variable to store the count value
+  int _count = 0;
 
-  int _count =0;
+  // Getter to access the count value from outside the class
   int get count => _count;
 
-  void increment(){
-    _count++;
-    update();
+  // Method to increment the count value
+  void increment() {
+    _count++; // Increase the count by 1
+    update(); // Notify listeners (e.g., GetBuilder) to rebuild the UI
   }
-
-
 }
